0002362c071dc7c70bf849a73e5451175e9bfed0
JASPER SHARMA <96339698+Pettyman123@users.noreply.github.com>
2024-05-18 20:56:17 +0530

    Fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+
+def fizzBuzz( n: int) -> list[str]:
+    l = []
+    for i in range(1,n+1):
+        if i%3==0 and i%5==0:
+            l.append("FizzBuzz")
+
+        elif i%3!=0 and i%5!=0:
+            l.append(str(i))
+        elif i%3==0:
+            l.append("Fizz")
+        elif i%5==0:
+            l.append("Buzz")        
+    return l
+    
+fizzBuzz(n=6)
